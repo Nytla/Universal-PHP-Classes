7fda14ab068b1e8c708ecedf331c0b37a5ebb285
nytla <nytla@mail.ru>
2012-12-06 15:15:57 +0200

    pdomysqlconnect_class

--- README.md
+++ README.md
@@ -4,10 +4,11 @@ This is my collection universal classes of php.
 ##The technologies used
 * PHP since v5.0
 
-##Contents of the collection
-* [Mailer](https://github.com/Nytla/Universal-PHP-Classes/blob/master/classes/Mailer.php) - this class send email, You can see an [example](https://github.com/Nytla/Universal-PHP-Classes/blob/master/examples/Mailer_example.php)
-* [ValidateData](https://github.com/Nytla/Universal-PHP-Classes/blob/master/classes/ValidateData.php) - this class validate data (boolean, email, float, integer, ip, url, string (login or password)), You can see an [example](https://github.com/Nytla/Universal-PHP-Classes/blob/master/examples/ValidateData_example.php)
-* [Cookie](https://github.com/Nytla/Universal-PHP-Classes/blob/master/classes/Cookie.php) - this class work with cookie (set, check, delete), You can see an [example](https://github.com/Nytla/Universal-PHP-Classes/blob/master/examples/Cookie_example.php)
+##Contents of the collection (alphabetically)
+* [Cookie.php](https://github.com/Nytla/Universal-PHP-Classes/blob/master/classes/Cookie.php) - this class work with cookie (set, check, delete), You can see an [example](https://github.com/Nytla/Universal-PHP-Classes/blob/master/examples/Cookie_example.php)
+* [Mailer.php](https://github.com/Nytla/Universal-PHP-Classes/blob/master/classes/Mailer.php) - this class send email, You can see an [example](https://github.com/Nytla/Universal-PHP-Classes/blob/master/examples/Mailer_example.php)
+* [PDOMysqlConnect.php](https://github.com/Nytla/Universal-PHP-Classes/blob/master/classes/PDOMysqlConnect.php) - this class connect to MYSQL (use PDO driver), You can see an [example](https://github.com/Nytla/Universal-PHP-Classes/blob/master/examples/PDOMysqlConnect_example.php)
+* [ValidateData.php](https://github.com/Nytla/Universal-PHP-Classes/blob/master/classes/ValidateData.php) - this class validate data (boolean, email, float, integer, ip, url, string (login or password)), You can see an [example](https://github.com/Nytla/Universal-PHP-Classes/blob/master/examples/ValidateData_example.php)
 
 ##Have comments or suggestions?
 Create [Issue](https://github.com/Nytla/Universal-PHP-Classes/issues).
---
+++ classes/PDOMysqlConnect.php
@@ -0,0 +1,154 @@
+<?php
+/**
+ * PDOMysqlConnect.php
+ *
+ * This is PDO MYSQL file
+ * 
+ * @category	classes
+ * @copyright	2012
+ * @author	Igor Zhabskiy <Zhabskiy.Igor@gmail.com>
+ */
+
+/**
+ * PDOMysqlConnect
+ * 
+ * This is PDO MYSQL Connect class
+ * 
+ * @version 0.1
+ */
+final class PDOMysqlConnect {
+
+	/**
+	 * _DB_driver
+	 * 
+	 * @var string	This is name of Database driver
+	 */
+	static private $_DB_driver = 'mysql';
+
+	/**
+	 * _DB_host
+	 * 
+	 * @var string	This is address of host our Database
+	 */
+	static private $_DB_host = 'localhost';
+
+	/**
+	 * _DB_port
+	 * 
+	 * @var string	This is port of protocol our Database
+	 */
+	static private $_DB_port = '3306';
+	
+	/**
+	 * _DB_name
+	 * 
+	 * @var string	This is name of Database
+	 */
+	static private $_DB_name = 'example_database';
+
+	/**
+	 * _DB_login
+	 * 
+	 * @var string	This is login of user our Database
+	 */
+	static private $_DB_login = 'root';
+
+	/**
+	 * _DB_password
+	 * 
+	 * @var string	This is password of user our Database
+	 */
+	static private $_DB_password = '';
+
+	/**
+	 * _DB_encoding
+	 * 
+	 * @var string	This is encoding of our Database
+	 */
+	static private $_DB_encoding = 'utf8';
+
+	/**
+	 * _DB_connect
+	 * 
+	 * @var string	This is connect to our Database
+	 */
+	static private $_DB_connect;
+
+	/**
+	 * dbConnect
+	 *
+	 * This function initialize connect to our Database
+	 *
+	 * @return object $_DB_connect	This is PDO connect mysql
+	 */
+	static public function dbConnect() {
+
+		/**
+		 * Create variable with connect data  
+		 */
+		$connect = self::$_DB_driver 
+			. ':host=' . self::$_DB_host 
+			. ';port=' . self::$_DB_port 
+			. ';dbname=' . self::$_DB_name;
+
+		try {
+			
+			/**
+			 * Create PDO object
+			 */
+			self::$_DB_connect = new PDO(
+				$connect,
+				self::$_DB_login,
+				self::$_DB_password,
+				array(
+					PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'' . self::$_DB_encoding . '\''
+				)
+			);
+
+			/**
+			 * Set attribute for DB connect
+			 */
+			self::$_DB_connect -> setAttribute(
+				PDO::ATTR_ERRMODE,
+				PDO::ERRMODE_EXCEPTION
+			);
+
+			return self::$_DB_connect;
+
+		} catch (PDOException $object) {
+
+			###############################################
+			# Good use of exceptions - is a error handling
+			###############################################
+			/**
+			 * If bad connection to MYSQL then write error message to errors.log
+			 */
+//			ErrorsLog::write($object -> getMessage());
+			
+			/**
+			 * Redirect to error page
+			 */
+//			Redirect::uriRedirect('bad_connect');
+
+
+			##############################################
+			# bad use of exceptions - is a return boolean
+			##############################################
+			/**
+			 * This is bad use of exceptions, but it is necessary for example
+			 */
+			return false;
+		}
+	}
+
+	/**
+	 * Destructor
+	 *
+	 * This is destructor close connect with Data Base
+	 */
+	public function __destruct() {
+
+		self::$_DB_connect = null;
+	}
+}
+?>
